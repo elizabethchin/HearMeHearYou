39f59e8dbbc7f81071c47ab3e93bdc7858e5e47c
Elizabeth Chin <elizabethtchin@gmail.com>
2019-07-03 03:55:59 +0000

    add password column

--- model.py
+++ model.py
@@ -21,30 +21,36 @@ class User(db.Model):
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    reports = db.relationship('Report')
 
     def __repr__(self):
         """Provide helpful representation when printed."""
         
         return ("<User user_id={} first_name={} last_name={} email={}>"
-            .format(self.user_id, self.fname, self.lname, self.email))
+            .format(self.user_id, self.first_name, self.last_name, self.email))
 
-class Rating(db.Model):
+class Report(db.Model):
     """List of reports."""
 
     __tablename__ = "reports"
 
     report_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    user_id = db.Column(db.relationship('User'), nullable=False)
+    user_id = db.Column(db.Intiger, db.ForeignKey('users.user_id'), nullable=False)
     date_time = db.Column(db.TIMESTAMPTZ, nullable=False)
     location = db.Column(db.String(100), nullable=True)
     witness = db.Column(db.String(100), nullable=True)
     anonymous = db.Column(db.Boolean, default=True)
     incident_descrip = db.Column(db.String(1000000), nullable=False)
 
+    user = db.relationship('User')
+
     def __repr__(self):
         """Provide helpful representation when printed."""
-        return ("<Report ID report_id={} user_id={} date_time={} location={} witness={} anonymous={} incident_descrip={}>"
-            .format(self.report_id, self.user_id, self.date_time self.location, self.witness,self.anonymous, self.incident_descrip))
+        return """<Report ID report_id={} user_id={} date_time={} location={}
+                witness={} anonymous={} incident_descrip={}>""".format(self.report_id, self.user_id, self.date_time, 
+                self.location, self.witness, self.anonymous, self.incident_descrip)
 
 ##############################################################################
 # Helper functions

d614b9eaf1a79e8badffe3160f693edfce5eeb96
Elizabeth Chin <elizabethtchin@gmail.com>
2019-08-09 22:21:10 -0700

    update app.jsx

--- templates/js/app.jsx
+++ templates/js/app.jsx
@@ -10,3 +10,14 @@ ReactDOM.render(
     <App />,
     document.getElementById("root")
 );
+
+class Todays_date extends React.Component {
+    render() {
+        return<p> Today's Date: {this.props.Todays_date}</p>
+    }
+}
+
+ReactDOM.render(
+    <Todays_date />,
+    document.getElementById("root")
+);

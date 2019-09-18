676b0446a62e90ad48bbcb3c1f3f20c215624d7f
Elizabeth Chin <elizabethtchin@gmail.com>
2019-09-18 01:58:13 -0700

    fix contact page

--- server.py
+++ server.py
@@ -223,7 +223,7 @@ def contact():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
     connect_to_db(app)
     # Use the DebugToolbar
     DebugToolbarExtension(app)
